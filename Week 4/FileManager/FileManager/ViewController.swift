f094a0c7793c49b3459598cde0fe84b02f168590
Consultant <consultant@Administrators-MacBook-Pro.local>
2022-06-27 08:03:43 -0400

    FileManager

--- Week 4/FileManager/FileManager/ViewController.swift
+++ Week 4/FileManager/FileManager/ViewController.swift
@@ -11,7 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let manager = FileManager.default
+        guard let url = manager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return
+        }
+        
+        let newFolder = url.appendingPathComponent("Test").appendingPathComponent("Second-Test")
+//        CREATE DIRECTORY
+//        do{
+////            try manager.createDirectory(at: newFolder, withIntermediateDirectories: true, attributes: [:])
+//
+//        }catch{
+//            print(error)
+//        }
+//        CREATE AND WRITE A FILE
+        let fileUrl = newFolder.appendingPathComponent("logs.txt")
+        let data = "Soy un texto".data(using: .utf8)
+        manager.createFile(atPath: fileUrl.path, contents: data, attributes: [FileAttributeKey.creationDate: Date()])
+//        DELETE FILE
+        if(manager.fileExists(atPath: fileUrl.path)){
+            do{
+                try manager.removeItem(at: fileUrl)
+            }catch{
+                print(error)
+            }
+        }
+       
     }
 
 
